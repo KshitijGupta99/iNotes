f92ff387337612bbf409cd9ee1e97bff17629a1b
KshitijGupta99 <guptakshitij111@gmail.com>
2024-08-25 14:00:27 +0530

    bug fixing

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,22 +8,16 @@ connectToMongo()
 const app = express()
 app.use(express.json())
 const corsOptions = {
-  origin: [
-    "https://i-notes-tajc.vercel.app",
-    "http://localhost:5173",
-    "https://inotes-kwax.onrender.com",
-  ],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization,auth-token",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+  res.header('Access-Control-Allow-Origin', 'https://i-notes-tajc.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,auth-token');
+  if (req.method === 'OPTIONS') {
+      return res.status(200).end(); // Handle preflight request
+  }
+  next();
+});
 
-app.use(cors(corsOptions));
-
-
-
-const port = process.env.PORT 
+const port = "https://inotes-kwax.onrender.com"
 app.get('/', (req, res) => {
   res.send('Hello Harry!')
 })
