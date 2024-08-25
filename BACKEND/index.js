75bfb495be97fa3545e038687887d96e539e3221
KshitijGupta99 <guptakshitij111@gmail.com>
2024-08-25 14:29:53 +0530

    token fixing

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,15 +8,29 @@ connectToMongo()
 const app = express()
 app.use(express.json())
 const corsOptions = {
-  res.header('Access-Control-Allow-Origin', 'https://i-notes-tajc.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,auth-token');
-  if (req.method === 'OPTIONS') {
-      return res.status(200).end(); // Handle preflight request
-  }
-  next();
-});
+  origin: [
+    "https://i-notes-tajc.vercel.app",
+    "http://localhost:5173",
+    "https://inotes-kwax.onrender.com"
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization,auth-token",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
+
+// Add your manual CORS handling middleware here
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://i-notes-tajc.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,auth-token');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end(); // Handle preflight request
+    }
+    next();
+});
 const port = "https://inotes-kwax.onrender.com"
 app.get('/', (req, res) => {
   res.send('Hello Harry!')
