6053e208b4665c38a6a227df9592196d8faba66e
KshitijGupta99 <guptakshitij111@gmail.com>
2024-08-26 16:29:15 +0530

    url changes

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,12 +8,27 @@ connectToMongo()
 const app = express()
 app.use(express.json())
 const corsOptions = {
+  ["https://i-notes-tajc.vercel.app", "local URL"],
+  origin: 'https://i-notes-tajc.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'auth-token'],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+// Use the CORS middleware with the options
+app.use(cors(corsOptions));
+
+// Add additional custom headers or handle OPTIONS requests
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://i-notes-tajc.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type,auth-token');
+  
   if (req.method === 'OPTIONS') {
-      return res.status(200).end(); // Handle preflight request
+    return res.status(200).end(); // Handle preflight request
   }
+
   next();
 });
 const port = "https://inotes-kwax.onrender.com"
