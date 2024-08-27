53aa49c28faa43733ef27e5a76e16d55aa68144f
KshitijGupta99 <guptakshitij111@gmail.com>
2024-08-27 19:00:47 +0530

    login error fix

--- BACKEND/middleware/fetchData.js
+++ BACKEND/middleware/fetchData.js
@@ -5,7 +5,7 @@ const JWT_SECRET = process.env.JWT_SECRET; // your json token signature
 const fetchData = (req, res, next) => {
     // Extract the token from the Authorization header
     let token = req.headers['auth-token'] || req.headers['authorization'];
-
+ 
     // Check if token is provided
     if (!token) {
         return res.status(401).json({ message: 'Token not provided' });
